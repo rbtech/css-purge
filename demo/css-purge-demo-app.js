d7224ee01e09972b3ba015191e09074f2c42113b
Andrew Quan <AndrewQ@RedBlueprint.com>
2018-08-02 14:00:32 +0200

    Updated demo to be self running

--- demo/css-purge-demo-app.js
+++ demo/css-purge-demo-app.js
@@ -1,26 +1,24 @@
 var	cssPurge = require('css-purge');
 
+//purging a CSS file with HTML and options
+cssPurge.purgeCSSFiles({
+  	css: 'test1.css', 
+  	html: 'html/test1.html',
+  	trim : true,
+  	shorten : true,
+  	verbose : true
+  }
+);
 
 //purging some CSS
 var css = "p { color: blue; color: blue; } ";
 cssPurge.purgeCSS(css, {
 	trim : true,
-	shorten : true
-	verbose : false,
+	shorten : true,
+	verbose : false
 }, function(error, result){
 	if (error)
 		console.log(error)
 	else
 		console.log('Output CSS: ',  result);
 });
-
-
-//purging a CSS file with HTML and options
-cssPurge.purgeCSSFiles({
-  	css: 'demo/test1.css', 
-  	html: 'demo/html/test1.html',
-  	trim : true,
-  	shorten : true,
-  	verbose : true
-  }
-);
